cdf4d552268178dd4c7c094dc5ddf0662ba69522
zenoxdev1 <67764269+zenoxdev1@users.noreply.github.com>
2023-06-03 18:54:17 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,22 +8,11 @@ import axios from 'axios'
 import moment from 'moment-timezone' 
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['50499698072', '💫 Mystic - Collaborator 1 💫', true],
-  ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
-  ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
-  ['593959425714', '💫 Mystic - Collaborator 4 💫', true],
-  ['50246028932', '💫 Mystic - Collaborator 5 💫', true],  
-  ['5212411719888', '💫 Mystic - Collaborator 6 💫', true],
-  ['5215533827255', '💫 Mystic - Collaborator 7 💫', false],
-  ['593968585283'],
-  ['5219993404349'],
-  ['584125778026'],
-  ['5219991402134'],	
-  ['5492266466080'],
-  ['5219996125657'],
-  ['5218442114446']
-] 
+  ['34603771642', 'Fate', true]
+  ]
+
+
+
 
 global.suittag = ['5219993404349'] 
 global.prems = ['51995386439'] 
